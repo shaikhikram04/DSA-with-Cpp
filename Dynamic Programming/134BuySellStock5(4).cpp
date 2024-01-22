a307ba31f178e60c47cc2e197b82fdeb8e75ba46
shaikhikram04 <120261083+shaikhikram04@users.noreply.github.com>
2024-01-22 18:07:21 +0530

    Optimal Approach

---
+++ Dynamic Programming/134BuySellStock5(4).cpp
@@ -0,0 +1,34 @@
+//! T.C -> O(n)
+//! S.C -> O(1)
+
+//* Optimal Approach
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        //* It store price that we have after buying a stock
+        //* if it is in -ve that means we are in loss
+        //* Obviously we try to store maximum value(to be in good profit)
+        int buy = INT_MIN;
+
+        //* It store profit
+        //* initially it is 0
+        int profit = 0;
+
+        //* iterate prices array
+        for (int price : prices) {
+            //* store max of previous buy price and 
+            //* price that left after buying curr stock
+            buy = max(buy, profit - price);
+
+            //* Store max previous profit prices and
+            //* price that left after sell curr stock with paying fee
+            profit = max(profit, buy + price - fee);
+        }
+
+        return profit;
+    }
+};
